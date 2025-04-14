7c9baff5ab9564054db1287b8599f2fa86426110
eduardpovazhnyi@gmail.com <eduardpovazhnyi@gmail.com>
2025-04-14 15:56:08 +0100

    Initial Commit

--- PowderCoatingApp/Form2.Designer.cs
+++ PowderCoatingApp/Form2.Designer.cs
@@ -35,6 +35,22 @@
             this.lblDateTime = new System.Windows.Forms.Label();
             this.timerDateTime = new System.Windows.Forms.Timer(this.components);
             this.btnBackToHome = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblPaymentInfo = new System.Windows.Forms.Label();
+            this.cmbPaymentMethod = new System.Windows.Forms.ComboBox();
+            this.btnRegister = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -110,6 +126,183 @@
             this.btnBackToHome.UseVisualStyleBackColor = true;
             this.btnBackToHome.Click += new System.EventHandler(this.btnBackToHome_Click);
             // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(219, 76);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(250, 31);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "Register New Account";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(220, 107);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(100, 28);
+            this.lblName.TabIndex = 11;
+            this.lblName.Text = "Full Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(225, 138);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(314, 34);
+            this.txtName.TabIndex = 12;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(221, 175);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(59, 28);
+            this.lblEmail.TabIndex = 13;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(226, 206);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(315, 34);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.BackColor = System.Drawing.Color.Transparent;
+            this.lblPhone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.ForeColor = System.Drawing.Color.White;
+            this.lblPhone.Location = new System.Drawing.Point(221, 243);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(144, 28);
+            this.lblPhone.TabIndex = 15;
+            this.lblPhone.Text = "Phone Number";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(226, 274);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(315, 34);
+            this.txtPhone.TabIndex = 16;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
+            this.lblPassword.Location = new System.Drawing.Point(220, 311);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(93, 28);
+            this.lblPassword.TabIndex = 17;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(226, 342);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(315, 34);
+            this.txtPassword.TabIndex = 18;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblConfirmPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassword.ForeColor = System.Drawing.Color.White;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(221, 379);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(168, 28);
+            this.lblConfirmPassword.TabIndex = 19;
+            this.lblConfirmPassword.Text = "Confirm Password";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(225, 410);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(314, 34);
+            this.txtConfirmPassword.TabIndex = 20;
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.BackColor = System.Drawing.Color.Transparent;
+            this.lblAddress.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.ForeColor = System.Drawing.Color.White;
+            this.lblAddress.Location = new System.Drawing.Point(578, 107);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(85, 28);
+            this.lblAddress.TabIndex = 21;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(583, 138);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(308, 34);
+            this.txtAddress.TabIndex = 22;
+            // 
+            // lblPaymentInfo
+            // 
+            this.lblPaymentInfo.AutoSize = true;
+            this.lblPaymentInfo.BackColor = System.Drawing.Color.Transparent;
+            this.lblPaymentInfo.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaymentInfo.ForeColor = System.Drawing.Color.White;
+            this.lblPaymentInfo.Location = new System.Drawing.Point(578, 175);
+            this.lblPaymentInfo.Name = "lblPaymentInfo";
+            this.lblPaymentInfo.Size = new System.Drawing.Size(134, 28);
+            this.lblPaymentInfo.TabIndex = 23;
+            this.lblPaymentInfo.Text = "Payment Info";
+            // 
+            // cmbPaymentMethod
+            // 
+            this.cmbPaymentMethod.BackColor = System.Drawing.Color.White;
+            this.cmbPaymentMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPaymentMethod.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPaymentMethod.FormattingEnabled = true;
+            this.cmbPaymentMethod.Location = new System.Drawing.Point(583, 204);
+            this.cmbPaymentMethod.Name = "cmbPaymentMethod";
+            this.cmbPaymentMethod.Size = new System.Drawing.Size(308, 36);
+            this.cmbPaymentMethod.TabIndex = 24;
+            this.cmbPaymentMethod.SelectedIndexChanged += new System.EventHandler(this.cmbPaymentMethod_SelectedIndexChanged);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.btnRegister.FlatAppearance.BorderSize = 5;
+            this.btnRegister.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.btnRegister.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnRegister.Location = new System.Drawing.Point(583, 370);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(308, 74);
+            this.btnRegister.TabIndex = 25;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
             // RegistrationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -118,6 +311,22 @@
             this.BackgroundImage = global::PowderCoatingApp.Properties.Resources.BackGroundImage2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(981, 618);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.cmbPaymentMethod);
+            this.Controls.Add(this.lblPaymentInfo);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnBackToHome);
             this.Controls.Add(this.lblDateTime);
             this.Controls.Add(this.lblAppName);
@@ -143,5 +352,21 @@
         private System.Windows.Forms.Label lblDateTime;
         private System.Windows.Forms.Timer timerDateTime;
         private System.Windows.Forms.Button btnBackToHome;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblPaymentInfo;
+        private System.Windows.Forms.ComboBox cmbPaymentMethod;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
--- PowderCoatingApp/Form2.cs
+++ PowderCoatingApp/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Security.Cryptography;
 using System.Drawing;
 using System.Linq;
@@ -23,8 +24,17 @@ namespace PowderCoatingApp
         private void RegistrationForm_Load(object sender, EventArgs e)
         {
             // Use the image from Resources
+            // Set background
             this.BackgroundImage = Properties.Resources.BackGroundImage2;
             this.BackgroundImageLayout = ImageLayout.Stretch;
+            timerDateTime.Start();
+
+            // Add items to Payment Method ComboBox
+            cmbPaymentMethod.Items.Add("Cash");
+            cmbPaymentMethod.Items.Add("Credit Card");
+            cmbPaymentMethod.Items.Add("Bank Transfer");
+            cmbPaymentMethod.Items.Add("PayPal");
+            cmbPaymentMethod.SelectedIndex = 0;
         }
 
         private void lblDateTime_Click(object sender, EventArgs e)
@@ -42,6 +52,95 @@ namespace PowderCoatingApp
             HomeForm homeForm = new HomeForm();
             await Animator.FadeOut(this);
             await Animator.FadeIn(homeForm);
+        }
+
+
+        // Add the Password Hashing Method
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void cmbPaymentMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // 1. Validate input fields
+            if (txtPassword.Text != txtConfirmPassword.Text)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtPassword.Text) ||
+                string.IsNullOrWhiteSpace(txtPhone.Text) ||
+                string.IsNullOrWhiteSpace(txtAddress.Text))
+            {
+                MessageBox.Show("Please fill in all required fields.");
+                return;
+            }
+
+            // 2. Hash password
+            string hashedPassword = HashPassword(txtPassword.Text);
+
+            // 3. Get selected payment method from ComboBox
+            string selectedPaymentMethod = cmbPaymentMethod.SelectedItem.ToString();
+
+            // 4. Connect to MySQL
+            string connectionString = "server=localhost;user=root;password=your_password;database=PowderCoatingDB;";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // 5. Insert into Users table
+                    string queryUser = @"INSERT INTO Users (name, email, password, phoneNumber, role) 
+                                 VALUES (@name, @email, @passwordHash, @phone, 'Customer')";
+                    MySqlCommand cmdUser = new MySqlCommand(queryUser, conn);
+                    cmdUser.Parameters.AddWithValue("@name", txtName.Text);
+                    cmdUser.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cmdUser.Parameters.AddWithValue("@passwordHash", hashedPassword);
+                    cmdUser.Parameters.AddWithValue("@phone", txtPhone.Text);
+                    cmdUser.ExecuteNonQuery();
+
+                    int userId = (int)cmdUser.LastInsertedId;
+
+                    // 6. Insert into Customers table
+                    string queryCustomer = @"INSERT INTO Customers (address, paymentInfo, userID) 
+                                     VALUES (@address, @paymentInfo, @userID)";
+                    MySqlCommand cmdCustomer = new MySqlCommand(queryCustomer, conn);
+                    cmdCustomer.Parameters.AddWithValue("@address", txtAddress.Text);
+                    cmdCustomer.Parameters.AddWithValue("@paymentInfo", selectedPaymentMethod);
+                    cmdCustomer.Parameters.AddWithValue("@userID", userId);
+                    cmdCustomer.ExecuteNonQuery();
+
+                    // 7. Show success and redirect
+                    MessageBox.Show("Registration successful! Please log in.");
+                    this.Hide();
+                    HomeForm home = new HomeForm();
+                    home.Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during registration: " + ex.Message);
+                }
+            }
         }
     }
 }
